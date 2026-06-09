aa6b27724a5ff8480a6afb977cc1fc2ca76db10a
uddesh pratap singh <uddeshsingh33@gmail.com>
2026-06-09 13:42:48 +0530

    Final changes in vehicle

--- src/main/java/com/uddesh/vehicle_scheduler/dto/Vehicle.java
+++ src/main/java/com/uddesh/vehicle_scheduler/dto/Vehicle.java
@@ -1,13 +1,25 @@
 package com.uddesh.vehicle_scheduler.dto;
 
-
 import lombok.Data;
 
 @Data
 public class Vehicle {
+
     private String TaskID;
 
     private int Duration;
 
     private int Impact;
+
+    public String getTaskID() {
+        return TaskID;
+    }
+
+    public int getDuration() {
+        return Duration;
+    }
+
+    public int getImpact() {
+        return Impact;
+    }
 }
